c22b3cd98151d3f0dc162b4b5da5b631cf6662d1
unknown <1107097431@qq.com>
2022-02-17 23:26:12 +0800

    feat:点餐界面功能完成大部分（差结算）

--- src/view/selects.vue
+++ src/view/selects.vue
@@ -1,44 +1,84 @@
 <script>
-    import { List, Avatar,Menu } from 'ant-design-vue'
+    import { List, Avatar,Menu, Button,Badge,Drawer } from 'ant-design-vue'
     import { mapGetters, mapActions } from 'vuex'
     export default {
         name: 'Selects',
         data() {
             return {
+                New:[
+                    {
+                        name:'珍珠奶茶',
+                        pictureUrl:'黑糖.jpeg',
+                        price:'￥12'
+                    }
+                ],
+                Fruit:[
+                    {
+                        name:'三拼霸霸奶茶',
+                        pictureUrl:'椰果.jpeg',
+                        price:'￥12'
+                    }                    
+                ],
+                Snacks:[
+                    {
+                        name:'霸霸椰果奶茶',
+                        pictureUrl:'椰果.jpeg',
+                        price:'￥12'
+                    }
+                ],
+                Milk:[
+
+                ],
+                Icecream:[],
+                Cheese:[],
+                Coffee:[],
+                Tea:[],
+                Ice:[],
                 menuList: [
                     {
                         name:'珍珠奶茶',
-                        pictureUrl:'黑糖.jpeg'
+                        pictureUrl:'黑糖.jpeg',
+                        price:'￥12'
                     }, 
                     {
                         name:'三拼霸霸奶茶',
-                        pictureUrl:'椰果.jpeg'
+                        pictureUrl:'椰果.jpeg',
+                        price:'￥12'
                     },  
                     {
                         name:'黑糖珍珠奶茶',
-                        pictureUrl:'黑糖.jpeg'
+                        pictureUrl:'黑糖.jpeg',
+                        price:'￥12'
                     },  
                     {
                         name:'霸霸椰果奶茶',
-                        pictureUrl:'椰果.jpeg'
+                        pictureUrl:'椰果.jpeg',
+                        price:'￥12'
                     },  
                     {
                         name:'双拼奶茶',
-                        pictureUrl:'双拼.jpeg'
+                        pictureUrl:'双拼.jpeg',
+                        price:'￥12'
                     },
                     {
                         name:'黑糖珍珠奶茶',
-                        pictureUrl:'黑糖.jpeg'
+                        pictureUrl:'黑糖.jpeg',
+                        price:'￥12'
                     },  
                     {
                         name:'霸霸椰果奶茶',
-                        pictureUrl:'椰果.jpeg'
+                        pictureUrl:'椰果.jpeg',
+                        price:'￥12'
                     },  
                     {
                         name:'双拼奶茶',
-                        pictureUrl:'双拼.jpeg'
+                        pictureUrl:'双拼.jpeg',
+                        price:'￥12'
                     },                                                                                                                           
-                ]
+                ],
+                shopping:[],
+                visible:false,
+                totalPrice:0
             };
         },
         mounted() {         
@@ -52,39 +92,73 @@
             ...mapActions({
                 set_userinfo: 'set_userinfo'
             }),
+            handleadd(itemName,itemPrice,itemAvater){
+                this.shopping.push({name:itemName,price:itemPrice,pictureUrl:itemAvater})
+                this.totalPrice = this.totalPrice + parseInt(itemPrice.replace('￥','')) 
+            },
+            onClose(){
+                this.visible = false
+            },
+            showDrawer(){
+                this.visible = true
+            }
         },
         render() {
             return (
                 <div class='select'>
                     <div class='menulist'>
                         <Menu mode="vertical" defaultSelectedKeys={['1']}>
-                            <Menu.Item key='1'><Avatar shape='square' src='清爽果茶.jpg'/>&nbsp;当季新品</Menu.Item>
-                            <Menu.Item key='2'><Avatar shape='square' src='清爽果茶.jpg'/>&nbsp;清爽果茶</Menu.Item>
-                            <Menu.Item key='3'><Avatar shape='square' src='小零食.jpeg'/>&nbsp;小零食</Menu.Item>
-                            <Menu.Item key='4'><Avatar shape='square' src='现煮奶茶.jpeg'/>&nbsp;现煮奶茶</Menu.Item>
-                            <Menu.Item key='5'><Avatar shape='square' src='鲜冰激凌.jpeg'/>&nbsp;鲜冰激凌</Menu.Item>
-                            <Menu.Item key='6'><Avatar shape='square' src='芝士奶盖.jpeg'/>&nbsp;芝士奶盖</Menu.Item>
-                            <Menu.Item key='7'><Avatar shape='square' src='新品咖啡.jpeg'/>&nbsp;新品咖啡</Menu.Item>
-                            <Menu.Item key='8'><Avatar shape='square' src='原叶纯茶.jpeg'/>&nbsp;原叶纯茶</Menu.Item>
-                            <Menu.Item key='9'><Avatar shape='square' src='冰沙系列.jpeg'/>&nbsp;冰沙系列</Menu.Item>
+                            <Menu.Item key='1' onClick={()=>{this.menuList = this.New}}><Avatar shape='square' src='清爽果茶.jpg'/>&nbsp;当季新品</Menu.Item>
+                            <Menu.Item key='2' onClick={()=>{this.menuList = this.Fruit}}><Avatar shape='square' src='清爽果茶.jpg'/>&nbsp;清爽果茶</Menu.Item>
+                            <Menu.Item key='3' onClick={()=>{this.menuList = this.Snacks}}><Avatar shape='square' src='小零食.jpeg'/>&nbsp;小零食</Menu.Item>
+                            <Menu.Item key='4' onClick={()=>{this.menuList = this.Milk}}><Avatar shape='square' src='现煮奶茶.jpeg'/>&nbsp;现煮奶茶</Menu.Item>
+                            <Menu.Item key='5' onClick={()=>{this.menuList = this.Icecream}}><Avatar shape='square' src='鲜冰激凌.jpeg'/>&nbsp;鲜冰激凌</Menu.Item>
+                            <Menu.Item key='6' onClick={()=>{this.menuList = this.Cheese}}><Avatar shape='square' src='芝士奶盖.jpeg'/>&nbsp;芝士奶盖</Menu.Item>
+                            <Menu.Item key='7' onClick={()=>{this.menuList = this.Coffee}}><Avatar shape='square' src='新品咖啡.jpeg'/>&nbsp;新品咖啡</Menu.Item>
+                            <Menu.Item key='8' onClick={()=>{this.menuList = this.Tea}}><Avatar shape='square' src='原叶纯茶.jpeg'/>&nbsp;原叶纯茶</Menu.Item>
+                            <Menu.Item key='9' onClick={()=>{this.menuList = this.Ice}}><Avatar shape='square' src='冰沙系列.jpeg'/>&nbsp;冰沙系列</Menu.Item>
                         </Menu>
                     </div>
                     <div class='drinklist'>
                         <List
-                            itemLayout='horizontal'
                             dataSource={this.menuList}
                             size='large'
                             renderItem={item =>(
                                 <div onClick={()=>{console.log('ok')}} style={{paddingLeft:'10px'}}>
-                                    <List.Item>
+                                    <List.Item actions={[<Button size='small' type='primary' onClick={this.handleadd.bind(this,item.name,item.price,item.pictureUrl)}>添加</Button>]}>
                                         <List.Item.Meta
                                             avatar={<Avatar src={item.pictureUrl}/>}
                                             title={item.name}
+                                            description={item.price}
                                         />
                                     </List.Item>
                                 </div>
                             )}
-                        ></List>                    
+                        ></List>
+                        <div class='shopping'><Badge count={this.shopping.length}><Avatar size='large' src='购物车.png' onClick={this.showDrawer}/></Badge></div>
+                        <Drawer visible={this.visible} onClose={this.onClose} width='400' closable={false}>
+                            <div class='drawer-content'>
+                                <List
+                                    dataSource={this.shopping}
+                                    renderItem={(item,index) =>(
+                                        <div style={{paddingLeft:'10px'}}>
+                                            <List.Item actions={[<Button size='small' type='primary' onClick={()=>{this.shopping.splice(index,1),this.totalPrice = this.totalPrice - parseInt(item.price.replace('￥',''))}}>删除</Button>]}>
+                                                <List.Item.Meta
+                                                    avatar={<Avatar src={item.pictureUrl}/>}
+                                                    title={item.name}
+                                                    description={item.price}
+                                                />
+                                            </List.Item>
+                                        </div>                                    
+                                    )}
+                                > 
+                                </List>                            
+                            </div>
+                            <div style={{paddingTop:'20px'}}>
+                                <div style={{float:'left'}}><span style={{display:'inline-block',paddingTop:'14px',fontSize:'20px',color:'#f40'}}>总价&nbsp;&nbsp;{this.totalPrice}</span></div>
+                                <div style={{float:'right'}}><Button size='large' type='primary'>结算</Button></div>
+                            </div>
+                        </Drawer>                  
                     </div>
                 </div>
             )
@@ -104,6 +178,20 @@
     .drinklist{
         height: 100%;
         background: rgb(255, 255, 255);
+        overflow-x: none;
+        overflow-y:scroll;
+        .shopping{
+            position: fixed;
+            bottom:10px;
+            right:50px;
+        }
+    }
+    .ant-drawer-body{
+        padding-top:20px;
+        height:100%;
+    }
+    .drawer-content{
+        height:90%;
         overflow-x: none;
         overflow-y:scroll;
     }
